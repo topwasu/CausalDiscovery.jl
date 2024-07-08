8423ada7817817576e323e3a796be8e3de7e50c3
topwasu <wasu.piri@gmail.com>
2024-07-08 17:18:41 -0400

    Fix install file

--- install_julia_packages.py
+++ install_julia_packages.py
@@ -1,5 +1,3 @@
 from juliacall import Pkg # if there's an error after this command, just try the command again
 Pkg.activate('.')
-Pkg.add(url="https://github.com/riadas/Autumn.jl#cs-eachindex") # suppose to get precompilation errors here
-Pkg.add(url='https://github.com/topwasu/SExpressions.jl.git')
 Pkg.update()
